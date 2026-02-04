63555391d7b435a124c85cc5f3166c755a7e7da1
Ilia-ran <ikrangelov24b@gmail.com>
2026-02-04 13:35:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 import streamlit as st
 
-st.title("Мини формуляр")
+st.title("Мини формуляр – Тест")
 
-question = "Коя е столицата на България?"
-correct_answer = "София"
-
-user_answer = st.text_input(question)
+q1 = st.text_input("Коя е столицата на България?")
+q2 = st.text_input("Кой континент е България?")
+q3 = st.text_input("През коя година е Освобождението на България?")
+q4 = st.text_input("Коя река минава през Русе?")
+q5 = st.text_input("Кой е авторът на 'Под игото'?")
 
 if st.button("Провери"):
-    if user_answer.strip().lower() == correct_answer.lower():
-        st.success("Верен отговор.")
+    correct = 0
+
+    if q1.strip().lower() == "софия":
+        correct += 1
+
+    if q2.strip().lower() == "европа":
+        correct += 1
+
+    if q3.strip().lower() == "1878":
+        correct += 1
+
+    if q4.strip().lower() == "дунав":
+        correct += 1
+
+    if q5.strip().lower() == "иван вазов":
+        correct += 1
+
+    if correct == 5:
+        st.success("Всички отговори са верни.")
     else:
-        st.error("Грешен отговор.")
+        st.error(f"Верни отговори: {correct} от 5")
